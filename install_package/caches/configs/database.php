a208e69b0bf3f44ad9d737cca4a5ce57b7567eb7
chenwei <chenwei@adnonstop.com>
2019-08-01 11:30:37 +0800

    更新数据库地址

--- install_package/caches/configs/database.php
+++ install_package/caches/configs/database.php
@@ -2,13 +2,14 @@
 
 return array (
 	'default' => array (
-		'hostname' => 'localhost',
+//		'hostname' => 'localhost',
+		'hostname' => '127.0.0.1',
 		'port' => 3306,
 		'database' => 'phpcmsv9',
-//		'username' => 'chenv_sql',
-		'username' => 'root',
-		'password' => '123456',
-//		'password' => 'VD$iU@qmgDEU',
+		'username' => 'chenv_sql',
+//		'username' => 'root',
+//		'password' => '123456',
+		'password' => 'VD$iU@qmgDEU',
 		'tablepre' => 'v9_',
 		'charset' => 'utf8',
 		'type' => 'mysql',
